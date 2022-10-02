879f99419132b831319df749c6f48226e88aa1ed
KayAhn0126 <dkseotn123@gmail.com>
2022-10-02 23:08:54 +0900

    refactor: 기존 MVC 구조를 MVVM으로 변경 후 파일 그룹화

---
+++ AppleFrameworkWithMVVM/ViewModel/FrameworkDetailViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  FrameworkDetailViewModel.swift
+//  AppleFrameworkWithMVVM
+//
+//  Created by Kay on 2022/10/02.
+//
+
+import Foundation
---
+++ AppleFrameworkWithMVVM/ViewModel/FrameworkViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  FrameworkViewModel.swift
+//  AppleFrameworkWithMVVM
+//
+//  Created by Kay on 2022/10/02.
+//
+
+import Foundation
